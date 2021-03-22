20b1043a3c4245bccda0a2cd5d2ed65124ba54a8
Deepak Warrier <dwarrier@gmail.com>
2021-03-21 18:42:27 -0700

    update get_block_info script to use start and target

--- get_block_info.py
+++ get_block_info.py
@@ -16,6 +16,9 @@ def flipBytes(hexStr):
   arr.reverse()
   return hexlify(arr).decode("utf-8")
 
+def concatData(jsonData):
+  return jsonData['ver'] + jsonData['prev_block'] + jsonData['mrkl_root'] + jsonData['time'] + jsonData['bits'] + jsonData['nonce']
+
 
 def parseBlockInfo(block):
   # This is signed
@@ -42,9 +45,8 @@ def parseBlockInfo(block):
   jsonData['bits'] = hexlify(bits.to_bytes(4,byteorder='little')).decode("utf-8")
   jsonData['nonce'] = hexlify(nonce.to_bytes(4,byteorder='little')).decode("utf-8")
 
-  return jsonData
+  return concatData(jsonData)
   
-
 def double_hash(hex_in):
   header_bin = unhexlify(hex_in)
   hash = hashlib.sha256(hashlib.sha256(header_bin).digest()).digest()
@@ -54,17 +56,34 @@ r = requests.get("https://api.blockcypher.com/v1/btc/main")
 t = json.loads(r.text)
 r = requests.get(t["latest_url"])
 # currBlock = json.loads(r.text)
-currBlock = json.loads(requests.get("https://api.blockcypher.com/v1/btc/main/blocks/00000000000022177691274561ebb697c01447998ce579f57212470b6837cf98").text)
+
+
+# 3 hours from target
+startBlockHash = "0000000000000c2312f81b4e7b8250f44dd905c7d3ab82466cc7bc28e7bce954"
+
+
+def getBlockFromHash(blockHash):
+  return json.loads(requests.get("https://api.blockcypher.com/v1/btc/main/blocks/" + blockHash).text)
+
+currBlock = getBlockFromHash(startBlockHash)
+targetBlockHash = "00000000000008a3a41b85b8b29ad444def299fee21793cd8b9e567eab02cd81"
 
 jsonMsg = {}
-jsonMsg['blocks'] = []
+jsonMsg['block_headers'] = []
 
-for i in range(4):
+found = False
+for i in range(6*8):
   data = parseBlockInfo(currBlock)
-  jsonMsg['blocks'].append(data)
+  print(data)
+  jsonMsg['block_headers'].insert(0,data)
   currBlock = getPrevBlock(currBlock)
+  if found:
+    break
+  if currBlock['prev_block'] == targetBlockHash:
+    found = True 
   
-print(json.dumps(jsonMsg,indent=4))
+# print(json.dumps(jsonMsg,indent=4))
+print(json.dumps(jsonMsg))
 
 '''
 # 2011 reference block
